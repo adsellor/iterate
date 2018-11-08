060ebc1931a48256a606a0bf199c460ca642274a
Minoris <zaven.snkhchyan@gmail.com>
2018-11-08 16:44:09 +0400

    add: flow support

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,7 @@ module.exports = {
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
     'gatsby-plugin-sass',
+    'gatsby-plugin-flow',
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
