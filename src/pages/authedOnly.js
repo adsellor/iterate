c782cc29f71a1fa38959444d627381c69f3855df
Minoris <zaven.snkhchyan@gmail.com>
2018-12-06 20:56:11 +0400

    remove: AuthProvider from authedOnly

--- src/pages/authedOnly.js
+++ src/pages/authedOnly.js
@@ -4,16 +4,13 @@ import { Router } from '@reach/router'
 import PrivateRoute from 'components/privateRoute'
 import Layout from 'components/layout'
 import Home from 'components/home'
-import AuthProvider from '../store/auth'
 
 const AuthedOnly = () => (
-  <AuthProvider>
-    <Layout>
-      <Router>
-        <PrivateRoute path="/home" component={Home} />
-      </Router>
-    </Layout>
-  </AuthProvider>
+  <Layout>
+    <Router>
+      <PrivateRoute path="/home" component={Home} />
+    </Router>
+  </Layout>
 )
 
 export default AuthedOnly
